1a5da4256a8a7d8390b82406ce78cc8d597bc67d
Rob <rnice4christ@gmail.com>
2018-12-21 08:43:08 -0600

    :zap: Update composer setup

--- dev-env-php72.sh
+++ dev-env-php72.sh
@@ -21,4 +21,18 @@ a2enmod php7.2
 
 ## Copmposer ##
 cd ~
-curl -sS https://getcomposer.org/installer -o composer-setup.php
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+exit $RESULT
