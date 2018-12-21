63adbbfd9e6c33bd0bf0114e2b9b79c948eda375
Rob <rnice4christ@gmail.com>
2018-12-21 09:59:55 -0600

    :zap: add php zip extension

--- dev-env-php72.sh
+++ dev-env-php72.sh
@@ -9,12 +9,12 @@ sudo apt-get install -y git xclip curl zip
 
 #Install Apache
 sudo apt update
-sudo apt install apache2
+sudo apt -y install apache2
 
 ## PHP ##
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
-sudo apt install php7.2 php7.2-cli php7.2-common php7.2-curl php7.2-dev libapache2-mod-php7.2 php7.2-gd php7.2-mbstring php7.2-json php7.2-mysql php7.2-readline php7.2-sqlite3
+sudo apt install -y php7.2 php7.2-cli php7.2-common php7.2-zip php7.2-curl php7.2-dev libapache2-mod-php7.2 php7.2-gd php7.2-mbstring php7.2-json php7.2-mysql php7.2-readline php7.2-sqlite3
 
 ##Enable PHP7.2 for apache
 a2enmod php7.2
